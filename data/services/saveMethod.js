e3ac77356253d3f8dc716f4c42180e944da6ddce
billhcmus <billgates15897@gmail.com>
2018-06-27 20:53:34 +0700

    update

--- data/services/saveMethod.js
+++ data/services/saveMethod.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const DOMParser = require('xmldom').DOMParser;
 const XMLSerializer = require('xmldom').XMLSerializer;
 const builder = require('xmlbuilder');
+const xml2js = require('xml2js');
 
 const pathSP = __dirname + '/../San_Pham'
 const pathPBH = __dirname + '/../Phieu_Ban_hang'
@@ -31,23 +32,30 @@ let changePrice = (data) => {
 let addSaleList = (data) => {
     let filePath = pathPBH + '/' + data.customerName + '.xml';
 
-    var bill = JSON.parse(data.billInfo);
-    var root = builder.create('ThongTinPhieu', {version: '1.0', encoding: 'UTF-8', standalone: true});
-    root.att('TenKhachHang', data.customerName);
-    root.att('SDT', data.phone);
-    root.att('DiaChi', data.address);
-    for (i in bill) {
-        var sp = root.ele('SanPham');
-        sp.att('MaSach', bill[i].MaSach);
-        sp.att('TenSach', bill[i].TenSach);
-        sp.att('SoLuong', bill[i].SoLuong);
-        sp.att('DonGia', bill[i].DonGia);
-        sp.att('TongTien', bill[i].TongTien);
-        sp.end({pretty: true});
-    }
-    root.end({pretty: true});
-    var dataWrite = root;
-    fs.writeFileSync(filePath, root, 'utf-8');
+    let xml = fs.readFileSync(filePath,'utf-8');
+
+    
+    var parse = new xml2js.Parser();
+    var root = builder.create(parse.parseString(xml));
+
+    console.log(root);
+    // var bill = JSON.parse(data.billInfo);
+    // var root = builder.create('ThongTinPhieu', {version: '1.0', encoding: 'UTF-8', standalone: true});
+    // root.att('TenKhachHang', data.customerName);
+    // root.att('SDT', data.phone);
+    // root.att('DiaChi', data.address);
+    // for (i in bill) {
+    //     var sp = root.ele('SanPham');
+    //     sp.att('MaSach', bill[i].MaSach);
+    //     sp.att('TenSach', bill[i].TenSach);
+    //     sp.att('SoLuong', bill[i].SoLuong);
+    //     sp.att('DonGia', bill[i].DonGia);
+    //     sp.att('TongTien', bill[i].TongTien);
+    //     sp.end({pretty: true});
+    // }
+    // root.end({pretty: true});
+    // var dataWrite = root;
+    // fs.writeFileSync(filePath, root, 'utf-8');
 }
 
 let changeStatus = (data) => {
